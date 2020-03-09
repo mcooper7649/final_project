d0da733c5356f80d9b02d4c7fb64f29802c7b87f
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2020-03-09 16:39:54 -0400

    gogog

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ console.log('Connecting DB to ', MONGODB_URI)
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .then((x) => console.log(`Connected to Mongo!!! Database name: "${x.connections[0].name}"`))
   .catch((err) => console.error('Error connecting to mongo', err));
 
 const app_name = require('./package.json').name;
